7e0ae40138c13b72a9409a3592a8a6451e115532
jwilyandi19 <jason16@mhs.if.its.ac.id>
2019-03-19 08:09:48 +0700

    Done

--- src/com/jwilyandi/pbkk/MulOperation.java
+++ src/com/jwilyandi/pbkk/MulOperation.java
@@ -2,7 +2,6 @@ package com.jwilyandi.pbkk;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
